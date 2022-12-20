6734d9db2b9a052972d859aba378dec96abf8f49
kabradshaw1 <itllbok82@gmail.com>
2022-12-20 11:04:57 -0500

    added mutations

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -21,6 +21,16 @@ const resolvers = {
     thought: async (parent, { _id }) => {
       return Thought.findOne({ _id });
     }
+  },
+  Mutation: {
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+
+      return user;
+    },
+    login: async () => {
+
+    }
   }
 };
 
